f761af1e49189cba10100b84e12bfb5d23b2001e
jofaco <jofaco1916@hotmail.com>
2024-04-17 08:53:55 -0500

    solucion a cors headers

--- videoteca/settings/base.py
+++ videoteca/settings/base.py
@@ -155,7 +155,9 @@ AUTH_USER_MODEL = 'users.User'
 
 CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
-
+CORS_ALLOW_HEADERS = [
+    'access-control-allow-origin',  # Asegúrate de incluir 'access-control-allow-origin' aquí
+]
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
