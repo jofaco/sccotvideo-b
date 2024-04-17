6de81b460a3229d20abb7b379bfae1fc7f4da455
jofaco <jofaco1916@hotmail.com>
2024-04-16 19:58:59 -0500

    Update base.py

--- videoteca/settings/base.py
+++ videoteca/settings/base.py
@@ -157,11 +157,11 @@ SIMPLE_JWT = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/videoteca/static/'
 STATIC_ROOT = BASE_DIR / 'static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/videoteca/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
