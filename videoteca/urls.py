348e111a3d81eacc130580937a3663c851708a44
jofaco <jofaco1916@hotmail.com>
2024-04-22 11:50:51 -0500

    Update urls.py

--- videoteca/urls.py
+++ videoteca/urls.py
@@ -30,7 +30,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('docspruebas/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
