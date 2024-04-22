e7364241bd914a91aa6b71e0cec8ecd5595b3b6a
jofaco <jofaco1916@hotmail.com>
2024-04-21 21:25:20 -0500

    changes

--- apps/users/api/viewsets/general_views.py
+++ apps/users/api/viewsets/general_views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from django.http import HttpResponse
 
 from rest_framework.parsers import MultiPartParser, FormParser
 
--- apps/users/views.py
+++ apps/users/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.permissions import IsAuthenticated
+from django.http import HttpResponse
 
 from apps.users.api.serializers.User_serializers import CustomTokenOptainPairSerializer, CustomUserSerializer
 from apps.users.models import User
@@ -166,4 +167,5 @@ class logout(GenericAPIView):
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
     
-
+def holaMundo(request):
+        return HttpResponse([{'name':"marlong"}])
