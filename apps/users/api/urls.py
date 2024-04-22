e8d026337ccbb2cb625b96a16d0c54faa15186d5
jofaco <jofaco1916@hotmail.com>
2024-04-21 21:27:25 -0500

    Update urls.py

--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 #from apps.users.api.viewset import  user_api_view, user_detail_api_view
 from apps.users.views import  logout, login,holaMundo
 from django.views.generic import TemplateView 
+
 urlpatterns = [
     #path('users/', user_api_view, name='user_api'),
     path('login/', login.as_view(), name='login'),
     path('logout/', logout.as_view(), name='logout'),
      path('app/',TemplateView.as_view(template_name='../../../build/index.html')),
-    path("saludo/", holaMundo.as_view(), name="saludo"),
+    path("saludo/", holaMundo, name="saludo"),
     #path('refresh-token/', userToken.as_view(), name='refresh_token'),
     #path('users/<int:pk>/', user_detail_api_view, name='user_detail_api_view')
 ]
