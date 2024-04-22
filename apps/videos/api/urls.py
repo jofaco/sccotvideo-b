eee633c5178ffa4fc7bb9318d38447996cf78582
jofaco <jofaco1916@hotmail.com>
2024-04-21 22:45:21 -0500

    Update urls.py

--- apps/videos/api/urls.py
+++ apps/videos/api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from apps.videos.api.viewsets.video_views import   VideoRetrieveAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from apps.videos.api.views import HolaMundoAPIView
 urlpatterns = [
     path('videos/retrieve/<int:pk>', VideoRetrieveAPIView.as_view(), name = 'video-detail'),
     path('hola-mundo/', HolaMundoAPIView.as_view(), name='hola_mundo'),
