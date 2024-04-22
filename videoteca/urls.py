ff369991b2e16df92a77973d918f361425b6f9cc
jofaco <jofaco1916@hotmail.com>
2024-04-21 21:39:10 -0500

    change

--- videoteca/urls.py
+++ videoteca/urls.py
@@ -3,7 +3,9 @@ from django.urls import path,include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-
+from django.urls import path
+from django.http import JsonResponse
+from . import views
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -29,6 +31,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('prueba/', views.prueba, name='prueba'),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
