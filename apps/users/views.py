530ac044036a4a16ef38328bc734201cb1996eaf
jofaco <jofaco1916@hotmail.com>
2024-06-04 15:29:17 -0500

    login con usuarios de odoo al dia y administrador correcto

--- apps/users/views.py
+++ apps/users/views.py
@@ -18,6 +18,20 @@ class login(TokenObtainPairView):
     serializer_class = CustomTokenOptainPairSerializer
     print("entro al login")
     def post(self, request, *args, **kwargs):
+        # Credenciales Odoo para conexion
+        url = "https://sccot.odoo.com"
+        db = "sccot-master-6280105"
+        #url = "https://sccot-tests-0305-12088670.dev.odoo.com"
+        #db = "sccot-tests-0305-12088670"
+        usernameOdoo = "desarrollador.web@sccot.org.co" 
+        #key = "Codig@2023"
+        #key = "d5fc1c1e9d85cf0827c06f3eb695820df719d641"
+        #url = "https://sccot.odoo.com"
+        #db = "sccot"
+        #usernameOdoo = "desarrollador.web@sccot.org.co" 
+        key = "ff15f623ef77dbfccd570420830edd64f9334c5a"
+        #key = "009d8fa50f7872c08c233726209a4b8a3af625b0"
+        #key = "0c256e03fe71e273993a4c3501722b0384f77627"
         """Metodo para realizar el login por parte del usuario
 
         Args.
@@ -38,12 +52,62 @@ class login(TokenObtainPairView):
             print("Usuario ya existente. validacion 1")
             
             login_serializer = self.serializer_class(data= request.data)
+# Obtener usuarios de odoo y comparar datos
 
-            # Validamos si el usuario esta Al día en odoo
-            if contact_details[0]["x_studio_estado_del_miembro"] == "Al día":
+            common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+            uid = common.authenticate(db,usernameOdoo,key,{})
+            models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
 
+            # Buscando usuario odoo que contenga numero de documento y email a buscar            
+            email_search = str(username).strip()
+            document_number = str(password).strip()
+            print(email_search,document_number)
+            search_domain = [('document_number', '=', document_number ), ('email', 'like',email_search )]
+            contact_ids = models.execute_kw(db, uid, key, 'res.partner', 'search', [search_domain])
+
+            if contact_ids:
+                contact_details = models.execute_kw(db, uid, key, 'res.partner', 'read', [contact_ids[0]])
+                # Validamos si el usuario esta Al día en odoo
+                # Validar categoria del miembro etc
+                if (
+                        contact_details[0]["x_studio_estado_del_miembro"] == "Al día" and
+                        (
+                            contact_details[0]["x_studio_categora_del_miembro"][1] == "Titular" or
+                            contact_details[0]["x_studio_categora_del_miembro"][1] == "Titular Senior" or
+                            contact_details[0]["x_studio_categora_del_miembro"][1] == "Honorario" or
+                            contact_details[0]["x_studio_categora_del_miembro"][1] == "Correspondiente" or
+                            contact_details[0]["x_studio_categora_del_miembro"][1] == "Residente" or
+                            contact_details[0]["x_studio_categora_del_miembro"][1] == "Adherente" or
+                            contact_details[0]["x_studio_categora_del_miembro"][1] == "Correspondiente Colombiano"
+                        ) and
+                        (
+                            contact_details[0]["x_studio_clase_de_cliente"][1] == "Miembro" or
+                            contact_details[0]["x_studio_clase_de_cliente"][1] == "Registro"
+                        )
+                    ):
+
+                    login_serializer = self.serializer_class(data= request.data)
+                    # Si el usuario esta al dia se realiza validacion de login
+                    if login_serializer.is_valid():
+
+                        user_serializer = CustomUserSerializer(user)
+                        return Response({
+                            'access_token': login_serializer.validated_data.get('access'),
+                            'refresh_token': login_serializer.validated_data.get('refresh'),
+                            'user': user_serializer.data,
+                            'message':'Inicio de sesión exitoso!'
+                        }, status=status.HTTP_200_OK) 
+                    else:
+                        print("algun dato quedo mal ")
+                        return Response({'error':'Nombre de usuario o contraseña incorrecta incorrectos'}, status=status.HTTP_400_BAD_REQUEST)
+                else:
+                    return Response({'error':'Lo sentimos, se ha restringido el acceso'}, status=status.HTTP_400_BAD_REQUEST)
+
+            else:
+                print("No aparece en el sistema")
+                # validar si es superuser
                 login_serializer = self.serializer_class(data= request.data)
-                # Si el usuario esta al dia se realiza validacion de login
+                    # Si el usuario esta al dia se realiza validacion de login
                 if login_serializer.is_valid():
 
                     user_serializer = CustomUserSerializer(user)
@@ -55,30 +119,14 @@ class login(TokenObtainPairView):
                     }, status=status.HTTP_200_OK) 
                 else:
                     print("algun dato quedo mal ")
-                    return Response({'error':'Algun dato esta mal ingresado'}, status=status.HTTP_400_BAD_REQUEST)
-            else:
-                print("No esta al dia por favor volver a iniciar sesion ")
-                return Response({'error':'Lo sentimos no es posible el acceso con tu estado de miembro actual'}, status=status.HTTP_400_BAD_REQUEST)
+                    return Response({'error':'Nombre de usuario o contraseña incorrecta incorrectos'}, status=status.HTTP_400_BAD_REQUEST)
+
+                # -----------------------
         
             # -----------------------------------------
         else:
             print("se ejecuto el else de aqui")
             # Obtener usuarios de odoo y comparar datos
-
-            # Credenciales Odoo para conexion
-            url = "https://sccot.odoo.com"
-            db = "sccot-master-6280105"
-            #url = "https://sccot-tests-0305-12088670.dev.odoo.com"
-            #db = "sccot-tests-0305-12088670"
-            usernameOdoo = "desarrollador.web@sccot.org.co" 
-            #key = "Codig@2023"
-            #key = "d5fc1c1e9d85cf0827c06f3eb695820df719d641"
-            #url = "https://sccot.odoo.com"
-            #db = "sccot"
-            #usernameOdoo = "desarrollador.web@sccot.org.co" 
-            key = "ff15f623ef77dbfccd570420830edd64f9334c5a"
-             #key = "009d8fa50f7872c08c233726209a4b8a3af625b0"
-            #key = "0c256e03fe71e273993a4c3501722b0384f77627"
 
             common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
             uid = common.authenticate(db,usernameOdoo,key,{})
@@ -94,67 +142,90 @@ class login(TokenObtainPairView):
             if contact_ids:
                 contact_details = models.execute_kw(db, uid, key, 'res.partner', 'read', [contact_ids[0]])
                 print(contact_details[0]["name"])
-                if contact_details[0]["x_studio_estado_del_miembro"] == "Al día" and contact_details[0]["x_studio_categora_del_miembro"][1] == "Titular" or contact_details[0]["x_studio_categora_del_miembro"][1] == "Titular Senior" or contact_details[0]["x_studio_categora_del_miembro"][1] == "Honorario" or contact_details[0]["x_studio_categora_del_miembro"][1] == "Correspondiente" or contact_details[0]["x_studio_categora_del_miembro"][1] == "Correspondiente Colombiano" and contact_details[0]["x_studio_clase_de_cliente"][1] == "Miembro" or contact_details[0]["x_studio_clase_de_cliente"][1] == "Registro":
-                    hashed_password = make_password(password)
-                    print(hashed_password) #Cambiar el contact detail de email a name user
-                    created = User.objects.get_or_create(username=contact_details[0]["email"],name=contact_details[0]["email"], defaults={'email': contact_details[0]["email"], 'password': hashed_password})
-                    userMod = authenticate(
-                        username = email_search,
-                        password = password
-                    )
-                    #creacion de un user
-                    if created:
-                        print("contraseña:",password.strip())
-                        print("correo:",username.strip())
-                        print("---------------Usuario creado exitosamente.----------")
-                        print("id del usuario",userMod)
-                        print("Nombre:", contact_details[0]["name"])
-                        print("Telefono:", contact_details[0]["phone"])
-                        print("Numero de documento:", contact_details[0]["document_number"])
-                        print("Correo:", contact_details[0]["email"])
-                        print("Estado miembro:", contact_details[0]["x_studio_estado_del_miembro"])
-                        print("Clase de cliente:", contact_details[0]["x_studio_clase_de_cliente"])
-                        login_serializer = self.serializer_class(data= request.data)
-                        print("se validara si se puede iniciar sesion")
-                        if login_serializer.is_valid():
-                            user_serializer = CustomUserSerializer(userMod)
-                            print("ya se envio el usuario para iniciar sesion")
-                            
-                            return Response({
-                                'access_token': login_serializer.validated_data.get('access'),
-                                'refresh_token': login_serializer.validated_data.get('refresh'),
-                                'user': user_serializer.data,
-                                'message':'Inicio de sesión exitoso!'
-                            }, status=status.HTTP_200_OK) 
+                if contact_details:
+                    if (
+                        contact_details[0]["x_studio_estado_del_miembro"] == "Al día" and
+                        (
+                            contact_details[0]["x_studio_categora_del_miembro"][1] == "Titular" or
+                            contact_details[0]["x_studio_categora_del_miembro"][1] == "Titular Senior" or
+                            contact_details[0]["x_studio_categora_del_miembro"][1] == "Honorario" or
+                            contact_details[0]["x_studio_categora_del_miembro"][1] == "Correspondiente" or
+                            contact_details[0]["x_studio_categora_del_miembro"][1] == "Residente" or
+                            contact_details[0]["x_studio_categora_del_miembro"][1] == "Adherente" or
+                            contact_details[0]["x_studio_categora_del_miembro"][1] == "Correspondiente Colombiano"
+                        ) and
+                        (
+                            contact_details[0]["x_studio_clase_de_cliente"][1] == "Miembro" or
+                            contact_details[0]["x_studio_clase_de_cliente"][1] == "Registro"
+                        )
+                    ):
+                        hashed_password = make_password(password)
+                        print(hashed_password) #Cambiar el contact detail de email a name user
+                        created = User.objects.get_or_create(username=contact_details[0]["email"],name=contact_details[0]["email"], defaults={'email': contact_details[0]["email"], 'password': hashed_password})
+                        userMod = authenticate(
+                            username = email_search,
+                            password = password
+                        )
+                        #creacion de un user
+                        if created:
+                            print("contraseña:",password.strip())
+                            print("correo:",username.strip())
+                            print("---------------Usuario creado exitosamente.----------")
+                            print("id del usuario",userMod)
+                            print("Nombre:", contact_details[0]["name"])
+                            print("Telefono:", contact_details[0]["phone"])
+                            print("Numero de documento:", contact_details[0]["document_number"])
+                            print("Correo:", contact_details[0]["email"])
+                            print("Estado miembro:", contact_details[0]["x_studio_estado_del_miembro"])
+                            print("Clase de cliente:", contact_details[0]["x_studio_clase_de_cliente"])
+                            login_serializer = self.serializer_class(data= request.data)
+                            print("se validara si se puede iniciar sesion")
+                            if login_serializer.is_valid():
+                                user_serializer = CustomUserSerializer(userMod)
+                                print("ya se envio el usuario para iniciar sesion")
+                                
+                                return Response({
+                                    'access_token': login_serializer.validated_data.get('access'),
+                                    'refresh_token': login_serializer.validated_data.get('refresh'),
+                                    'user': user_serializer.data,
+                                    'message':'Inicio de sesión exitoso!'
+                                }, status=status.HTTP_200_OK) 
+                            else:
+                                print("ya se valido y se dio o dara una respuesta")
+                                return Response({'error':'Tienes novedades con tu usuario'}, status=status.HTTP_400_BAD_REQUEST)
+                                
                         else:
+                            print("Usuario ya existente.")
+                            login_serializer = self.serializer_class(data= request.data)
 
-                            print("ya se valido y se dio o dara una respuesta")
-                            return Response({'error':'Contraseña o nombre de usuario incorrectos'}, status=status.HTTP_400_BAD_REQUEST)
-                    else:
-                        print("Usuario ya existente.")
-                        login_serializer = self.serializer_class(data= request.data)
-
-                        if login_serializer.is_valid():
-                            user_serializer = CustomUserSerializer(user)
-                            
-                            return Response({
-                                'access_token': login_serializer.validated_data.get('access'),
-                                'refresh_token': login_serializer.validated_data.get('refresh'),
-                                'user': user_serializer.data,
-                                'message':'Inicio de sesión exitoso!'
-                            }, status=status.HTTP_200_OK) 
+                            if login_serializer.is_valid():
+                                user_serializer = CustomUserSerializer(user)
+                                
+                                return Response({
+                                    'access_token': login_serializer.validated_data.get('access'),
+                                    'refresh_token': login_serializer.validated_data.get('refresh'),
+                                    'user': user_serializer.data,
+                                    'message':'Inicio de sesión exitoso!'
+                                }, status=status.HTTP_200_OK) 
                         # return Response({'error':'Contraseña o nombre de usuario incorrectos'}, status=status.HTTP_400_BAD_REQUEST)
+                    else:
+                        # Validar si es una empresa y validar campo Acceso SCCOT y crearle el usuario
+
+                        print("validar si es una empresa")
+                        print("Tienes novedades con tu usuario", password, "not found.")
+                        return Response({'error':'Tienes novedades con tu usuario'}, status=status.HTTP_400_BAD_REQUEST)
                 else:
-                    print("Todavia no estas al dia por favor pagar anualidad del año", password, "not found.")
-                    return Response({'error':'Todavia no estas al dia por favor pagar anualidad del año'}, status=status.HTTP_400_BAD_REQUEST)
+                
+                    print("No hay detalles del usuario")
             else:
+            
                 print("Contacto no existe en nuestro sistema", password, "not found.")
                 print("cualquier string",type("hola"))
                 print("correo string",type(email_search))
                 print("correo string",type(username))
                 print("password string",type(password))
-                return Response({'error':'nombre de usuario incorrectos'}, status=status.HTTP_400_BAD_REQUEST)
-
+                return Response({'error':'Nombre de usuario o contraseña incorrecta incorrectos'}, status=status.HTTP_400_BAD_REQUEST)
+    
 
 
 
