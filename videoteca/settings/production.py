57bd745a64afbd8e35deb7f320e8bfb52e4f5705
jofaco <jofaco1916@hotmail.com>
2024-07-15 15:59:55 -0500

    prueba videoForm con errores

--- videoteca/settings/production.py
+++ videoteca/settings/production.py
@@ -16,7 +16,7 @@ DATABASES = {
        #local
         'NAME': 'sccotvideo041204', 
         #'USER':'root',
-        #'PASSWORD':'',
+        'PASSWORD':'',
         #'HOST':'localhost',
         #'PORT':'3306',
         #production
