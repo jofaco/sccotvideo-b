8badcd23337113fe4a78b3902cd1b5dd85cb3bbc
jofaco <36674053+jofaco@users.noreply.github.com>
2025-09-14 21:39:21 -0500

    se cambia conexion db

--- videoteca/settings/production.py
+++ videoteca/settings/production.py
@@ -11,38 +11,38 @@ ALLOWED_HOSTS = ["*"]
 
 # PARA PRODUCCION
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'OPTIONS': {
-#             'sql_mode': 'traditional',
-#         },
-#        #local
-#         'NAME': 'sccotvideo041204',
-#         'USER':'root',
-#         #'HOST':'localhost',
-#         #'PORT':'3306',
-#         #production
-#         #'NAME': 'sccotvideo0122024',
-#         'USER':'root',
-#         'PASSWORD':'',
-#         #'PASSWORD':'123Password',
-#         'HOST':'localhost',
-#         'PORT':'3306'
-#     }
-# }
-
-# PARA DESARROLLO
 DATABASES = {
     'default': {
-        'ENGINE': 'mysql.connector.django',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+        },
+       #local
         'NAME': 'sccotvideo041204',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        #'USER':'root',
+        #'HOST':'localhost',
+        #'PORT':'3306',
+        #production
+        #'NAME': 'sccotvideo0122024',
+        'USER':'root',
+        #'PASSWORD':'',
+        'PASSWORD':'123Password',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
+
+# PARA DESARROLLO
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'mysql.connector.django',
+#         'NAME': 'sccotvideo041204',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
